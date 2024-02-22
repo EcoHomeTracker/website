1c7538808d4326b25e4b58949ae699057391937f
Vladimir Aleksic <vlada.pzs@hotmail.com>
2024-02-22 21:05:19 +0100

    Add desktop navbar; update transitions (#10)

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -5,63 +5,72 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import { Bars3Icon, ChevronUpIcon } from '@heroicons/react/24/outline'
+import { useHide } from '@/hooks/useHide'
 
-import Button from './Button'
+import Button from '@components/Button'
+import classNames from 'classnames'
 
 export default function MobileMenu() {
   const showMenu = useSearchParams().get('showMenu') === 'true'
+  const completelyHidden = useHide(showMenu)
+
+  const variants = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0 },
+  }
 
   return (
-    <div
-      className='absolute top-0 z-20 block w-full pt-12 lg:hidden'
-      style={{ backgroundColor: 'rgb(30, 30, 30)' }}
-    >
-      <div className='flex w-full justify-between px-6'>
-        <Image
-          className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-          src='/vercel.svg'
-          alt='Next.js Logo'
-          width={120}
-          height={24}
-          priority
-        />
+    <div className='absolute top-0 z-20 block w-full lg:hidden'>
+      <div
+        className='flex w-full justify-between px-6 py-12'
+        style={{ backgroundColor: 'rgba(30, 30, 30)' }}
+      >
+        <div className='relative h-6 w-32'>
+          <Image
+            className='dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
+            src='/vercel.svg'
+            alt='Next.js Logo'
+            fill
+            priority
+          />
+        </div>
         <Link
-          className='flex h-9 w-9 cursor-pointer items-center justify-center rounded-md duration-300 hover:bg-gray-800'
+          className='flex h-9 w-9 cursor-pointer items-center justify-center rounded-md duration-300 hover:bg-gray-700'
           href={{ search: 'showMenu=' + !showMenu }}
         >
           {!showMenu && <Bars3Icon className='h-6 w-6' />}
           {showMenu && <ChevronUpIcon className='h-6 w-6' />}
         </Link>
       </div>
-      {showMenu && (
-        <motion.div
-          className='overflow-hidden rounded-b-xl px-6 py-4 shadow'
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-        >
-          <ul className='flex w-full flex-col gap-4 py-8'>
-            <Link href='#home' className='w-full cursor-pointer'>
-              <li>Home</li>
-            </Link>
-            <Link href='#features' className='w-full cursor-pointer'>
-              <li>Features</li>
-            </Link>
-            <Link href='#pricing' className='w-full cursor-pointer'>
-              <li>Pricing</li>
-            </Link>
-            <Link href='#reviews' className='w-full cursor-pointer'>
-              <li>Reviews</li>
-            </Link>
-          </ul>
-          <div className='flex flex-col gap-4'>
-            <Button
-              className='btn-secondary'
-              content='Subscribe to newsletter'
-            />
-            <Button className='btn-primary' content='Download the app' />
-          </div>
-        </motion.div>
-      )}
+      <motion.div
+        className={classNames(
+          'overflow-hidden rounded-b-xl px-6 py-4',
+          completelyHidden && 'hidden',
+        )}
+        variants={variants}
+        animate={showMenu ? 'visible' : 'hidden'}
+        transition={{ bounce: 0 }}
+        style={{ backgroundColor: 'rgba(30, 30, 30)' }}
+      >
+        <ul className='flex w-full flex-col gap-4 py-8'>
+          <Link href='#home' className='w-full cursor-pointer'>
+            <li>Home</li>
+          </Link>
+          <Link href='#features' className='w-full cursor-pointer'>
+            <li>Features</li>
+          </Link>
+          <Link href='#pricing' className='w-full cursor-pointer'>
+            <li>Pricing</li>
+          </Link>
+          <Link href='#reviews' className='w-full cursor-pointer'>
+            <li>Reviews</li>
+          </Link>
+        </ul>
+        <div className='flex flex-col gap-4'>
+          <Button className='btn-secondary' content='Subscribe to newsletter' />
+          <Button className='btn-primary' content='Download the app' />
+        </div>
+      </motion.div>
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,56 @@
 import { Suspense } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
 import MobileMenu from './MobileMenu'
+import Button from './Button'
 
 export default function Navbar() {
   return (
     <nav className='relative w-full'>
+      <div className='absolute left-0 right-0 top-0 mx-auto hidden w-full max-w-7xl items-center px-12 py-12 lg:flex'>
+        <div className='pr-12'>
+          <div className='relative h-6 w-32'>
+            <Image
+              className='dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
+              src='/vercel.svg'
+              alt='Next.js Logo'
+              fill
+              priority
+            />
+          </div>
+        </div>
+        <ul className='flex w-full flex-1 justify-start gap-8 py-8 text-gray-200'>
+          <Link
+            href='#home'
+            className='rounded-md px-4 py-2 text-sm duration-300 hover:bg-gray-700 hover:text-white'
+          >
+            <li>Home</li>
+          </Link>
+          <Link
+            href='#features'
+            className='rounded-md px-4 py-2 text-sm duration-300 hover:bg-gray-700 hover:text-white'
+          >
+            <li>Features</li>
+          </Link>
+          <Link
+            href='#pricing'
+            className='rounded-md px-4 py-2 text-sm duration-300 hover:bg-gray-700 hover:text-white'
+          >
+            <li>Pricing</li>
+          </Link>
+          <Link
+            href='#reviews'
+            className='rounded-md px-4 py-2 text-sm duration-300 hover:bg-gray-700 hover:text-white'
+          >
+            <li>Reviews</li>
+          </Link>
+        </ul>
+        <div className='flex flex-1 justify-end gap-4'>
+          <Button className='btn-secondary w-fit' content='Subscribe' />
+          <Button className='btn-primary w-fit' content='Download' />
+        </div>
+      </div>
       <Suspense>
         <MobileMenu />
       </Suspense>
